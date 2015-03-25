1c5bbf8168a12ee4e66bc8089f8e7ccd98f1bb7b
pmcgrath <pmcgrath@gmail.com>
2015-03-25 16:31:21 +0000

    Removed redundant xml parsing logic

--- src/sq/currency.clj
+++ src/sq/currency.clj
@@ -9,41 +9,8 @@
 (defn get-currency-iso-alpha-codes
   []
   ; See http://stackoverflow.com/questions/1194044/clojure-xml-parsing
+  ; See commit f95150520d95f1e82353bf8bce0cb59bd6784593 to see parsing the xml without clojure.data.zip.xml - git show 92589d043129466768089469dd0e19753d5caabc
   (let [data (xml/parse url)
         zipped (zip/xml-zip data)
         currency-codes  (zipxml/xml-> zipped :CcyTbl :CcyNtry :Ccy zipxml/text)]
     (-> currency-codes distinct sort)))
-
-(defn extract-countries-from-root
-  [root]
-  ; root xml is "<ISO_4217 Pblshd="2015-01-01"><CcyTbl><CcyNtry>...."
-  ; We want to a get a collection for the CcyTbl element
-  ; So we need to 
-  ;   get :content from root (map) which gives us a vector
-  ;   get the first item from the vector which gives us a map
-  ;   get :content from the map which gives us a vector (Each entry which is a map corresponds to the CcyNtry source element) 
-  (-> root :content first :content))
-
-(defn create-a-country-data-map
-  [country]
-  ; country xml is "<CcyNtry><CtryNm>AFGHANISTAN</CtryNm><CcyNm>Afghani</CcyNm><Ccy>AFN</Ccy><CcyNbr>971</CcyNbr><CcyMnrUnts>2</CcyMnrUnts></CcyNtry>"
-  ; We want to a get a map for the CcyNtry element content
-  ; So we need to
-  ;   get :content from incomming map which gives us a vector
-  ;   reduce the vector to a map where each vector entry results in a key based on :tag and a value based on the first element in the :content vector
-  (let [country-data-items (:content country)
-        reduction-fn (fn [accum country-data-item] (assoc accum (:tag country-data-item) (-> country-data-item :content first)))]
-    (reduce reduction-fn {} country-data-items)))
-
-(defn get-and-parse-countries
-  []
-  (let [data (xml/parse url)
-        countries (extract-countries-from-root data)]
-    (map create-a-country-data-map countries)))
-
-(defn get-currency-iso-alpha-codes-long-way-round
-  []
-  (let [countries (get-and-parse-countries)
-        currency-codes (map #(:Ccy %) countries)]
-    ; Distinct and all non nil currencies (Antartica has no currency)
-    (sort (remove nil? (distinct currency-codes)))))
